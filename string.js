c276c1c46d234b0ad27c23592ffa59ec512c5a97
Nithish124 <85726111+Nithish124@users.noreply.github.com>
2024-04-30 08:27:05 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,25 @@
+let name="Nithishwaran";
+console.log(name.length);
+//charAt()
+console.log(name.charAt(0));
+//charcodeAt()
+console.log(name.charCodeAt(0));
+//at()
+console.log(name.at(2));
+//slice()
+console.log(name.slice(2,5));
+//substring()
+console.log(name.substring(5,7));
+//touppercase()
+console.log(name.toUpperCase());
+//tolowercase()
+console.log(name.toLowerCase());
+//concat()
+let text1="Hello";
+let text2="world";
+let text3=text1.concat(" ",text2);
+console.log(text3);
+let str=" hello world ";
+console.log(str.trim());
+console.log(str.trimStart());
+console.log(str.trimEnd());
